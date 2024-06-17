799d85925b18b618f69cb0c4f167e63d0a6f4fd3
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-17 14:16:08 +0200

    Initial Commit

---
+++ SwiftUI-SA-BarcodeScanner/SwiftUI_SA_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SA_BarcodeScannerApp.swift
+//  SwiftUI-SA-BarcodeScanner
+//
+//  Created by Miran Hrupački on 17.06.2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SA_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
